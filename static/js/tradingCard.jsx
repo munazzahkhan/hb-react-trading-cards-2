7a576693966418123c7048069c6dc39c0f8446b2
Munazzah Hassan Khan <munazzah.hassan@gmail.com>
2021-05-20 17:20:28 -0700

    Completed Lab

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -29,9 +29,18 @@ function TradingCard(props) {
 }
 
 function TradingCardContainer() {
+
+  const [cards, updateCards] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('/cards.json')
+    .then((response) => response.json())
+    .then((data) => updateCards(data.cards))
+  }, []);
+
   const tradingCards = [];
 
-  for (const currentCard of tradingCardData) {
+  for (const currentCard of cards) {
     tradingCards.push(
       <TradingCard
         key={currentCard.name}
@@ -45,6 +54,7 @@ function TradingCardContainer() {
   return (
     <div>{tradingCards}</div>
   );
+
 }
 
 ReactDOM.render(
